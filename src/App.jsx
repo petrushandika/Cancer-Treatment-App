d7ff6eb6e845ce1532b3e62f3c01646d49a0550a
petrushandika <petrushandikasinaga@gmail.com>
2024-09-18 21:11:48 +0700

    style: sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-
 import { Route, Routes, useNavigate } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <div className="relative flex min-h-screen flex-row bg-[#13131a]">
-      <div className="relative mr-10 hidden sm:flex">{/* Sidebar */}</div>
+      <div className="relative ml-10 hidden sm:flex">
+        <Sidebar />
+      </div>
 
       <div className="mx-auto max-w-[1280px] flex-1 max-sm:w-full sm:pr-5">
         {/* Navbar */}
