fd2a43cd1be5f013a59872f5ee9dc42d943c8d2e
petrushandika <petrushandikasinaga@gmail.com>
2024-09-23 20:21:17 +0700

    feat: logic display onboarding component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import { Home, OnBoarding } from "./pages";
+import { useStateContext } from "./context";
+import { usePrivy } from "@privy-io/react-auth";
 
 const App = () => {
+  const { currentUser } = useStateContext();
+  const { user, authenticated, ready, login } = usePrivy();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (ready && !authenticated) {
+      login();
+    } else if (user && !currentUser) {
+      navigate("/onboarding");
+    }
+  }, [ready, currentUser, navigate]);
+
   return (
     <div className="relative flex min-h-screen flex-row bg-[#13131a]">
       <div className="relative ml-10 hidden sm:flex">
